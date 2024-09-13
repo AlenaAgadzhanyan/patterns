5bbea5b3267cd09b74c975d144b2197817dce1ff
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-09-13 19:56:00 +0300

    task 3 subtask 2

--- lab1/array.rb
+++ lab1/array.rb
@@ -27,6 +27,29 @@ def index_first_positive_element(array)
   return nil
 end
 
-array = [nil]
-puts "#{minimum(array)}"
-puts "#{index_first_positive_element(array)}"
+# Написать программу, которая принимает как аргумент два значения. Первое значение говорит, какой из методов задачи 
+# 1 выполнить, второй говорит о том, откуда читать список аргументом должен быть написан адрес файла. Далее 
+# необходимо прочитать массив и выполнить метод.
+
+def read_from_file(name_file)
+  array=[]
+  File.open(name_file, 'r') do |file|
+    file.each_line {|x| array.push(x.to_i)}
+  end
+  return array
+end
+
+if ARGV.length == 2 then
+  number_method = ARGV[0].to_i
+  name_file = ARGV[1]
+  array = read_from_file(name_file)
+  case number_method
+  when 1
+    puts "#{minimum(array)}"
+  when 2
+    puts "#{index_first_positive_element(array)}"
+  else
+    puts "Нет метода с таким номером"
+  end
+else puts "Введите 2 аргумента"
+end
