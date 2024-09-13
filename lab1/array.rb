18df37858a2973fc4c13b7c680acbacbcd241ad8
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-09-13 18:51:08 +0300

    task 3 subtask 1

---
+++ lab1/array.rb
@@ -0,0 +1,32 @@
+# Написать методы, которые находят минимальный, элементы, номер первого положительного элемента. Каждая 
+# операция в отдельном методе. Решить задачу с помощью циклов(for и while).
+
+def minimum(array)
+  if array.count{|item| !item.nil?} != 0 then
+    min = array[0]
+  else return "The array doesn't contain not-zero elements"
+  end
+  for index in 1...array.length do
+    if array[index] != nil && array[index] < min then
+      min = array[index]
+    end
+  end
+  return min
+end
+
+
+def index_first_positive_element(array)
+  if array.count{|item| !item.nil?} == 0 then
+    return "The array doesn't contain not-zero elements"
+  end
+  for index in 0...array.length do
+    if array[index] != nil && array[index] > 0 then
+      return index
+    end
+  end
+  return nil
+end
+
+array = [nil]
+puts "#{minimum(array)}"
+puts "#{index_first_positive_element(array)}"
