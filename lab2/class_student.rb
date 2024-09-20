c814513669771ba881ff4f364f04dd5400826142
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-09-20 18:26:41 +0300

    task 1 subtask 1

---
+++ lab2/class_student.rb
@@ -0,0 +1,81 @@
+class Student()
+
+  # Конструктор
+  def Initialize(id = nil, surname, firstname, lastname, phone_number = nil, telegram = nil, email = nil, git = nil)
+    @id = id
+    @surname = surname
+    @firstname = firstname
+    @lastname = lastname
+    @phone_number = phone_number
+    @telegram = telegram
+    @email = email
+    @git = git
+  end
+
+  # Геттеры
+  def id
+    @id
+  end
+
+  def surname
+    @surname
+  end
+
+  def firstname
+    @firstname
+  end
+
+  def lastname
+    @lastname
+  end
+
+  def phone_number
+    @phone_number
+  end
+
+  def telegram
+    @telegram
+  end
+
+  def email
+    @email
+  end
+
+  def git
+    @git
+  end
+
+  # Сеттеры
+  def id=(id_value)
+    @id = id_value
+  end
+
+  def surname=(surname_value)
+    @surname = surname_value
+  end
+
+  def firstname=(firstname_value)
+    @firstname = firstname_value
+  end
+
+  def lastname=(lastname_value)
+    @lastname = lastname_value
+  end
+
+  def phone_number=(phone_number_value)
+    @phone_number = phone_number_value
+  end
+
+  def telegram=(telegram_value)
+    @telegram = telegram_value
+  end
+
+  def email=(email_value)
+    @email = email_value
+  end
+
+  def git=(git_value)
+    @git = git_value
+  end
+
+end
