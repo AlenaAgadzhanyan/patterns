29295efb5134fe0435189c8f664054997db51252
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-09-24 23:11:54 +0300

    task 1 subtask 7

--- Student/class_student.rb
+++ Student/class_student.rb
@@ -1,18 +1,30 @@
 class Student
 
-  attr_accessor :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
-  
+  attr_accessor :surname, :firstname, :lastname, :id, :telegram, :email, :git
+  attr_reader :phone_number
+
   def initialize(parameters)
     @id = parameters[:id]
     @surname = parameters[:surname]
     @firstname = parameters[:firstname] 
     @lastname = parameters[:lastname]
-    @phone_number = parameters[:phone_number]
+    self.phone_number = parameters[:phone_number]
     @telegram = parameters[:telegram]
     @email = parameters[:email]
     @git = parameters[:git]
 
     raise "Please enter your surname, firstname, lastname" if @surname.nil? || @lastname.nil? || @firstname.nil?
+  end
+
+  def self.phone_number_regex?(phone_number_value)
+    phone_number_value.match(/\A(\+?7|8)\d{10}\z/)
+  end
+
+  def phone_number=(phone_number_value)
+    if !self.class.phone_number_regex?(phone_number_value) then
+      raise "Incorrect phone number"
+    end
+    @phone_number = phone_number_value
   end
 
   def information()
