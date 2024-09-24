276eddf7bdad6f5bf172931707f3500732b9d6dd
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-09-24 22:25:50 +0300

    task 1 subtask 5

--- Student/class_student.rb
+++ Student/class_student.rb
@@ -2,19 +2,29 @@ class Student
 
   attr_accessor :surname, :firstname, :lastname, :id, :phone_number, :telegram, :email, :git
   
-  def initialize(surname, firstname, lastname, id = nil, phone_number = nil, telegram = nil, email = nil, git = nil)
-    @id = id
-    @surname = surname
-    @firstname = firstname
-    @lastname = lastname
-    @phone_number = phone_number
-    @telegram = telegram
-    @email = email
-    @git = git
+  def initialize(parameters)
+    @id = parameters[:id]
+    @surname = parameters[:surname]
+    @firstname = parameters[:firstname] 
+    @lastname = parameters[:lastname]
+    @phone_number = parameters[:phone_number]
+    @telegram = parameters[:telegram]
+    @email = parameters[:email]
+    @git = parameters[:git]
+
+    raise "Please enter your surname, firstname, lastname" if @surname.nil? || @lastname.nil? || @firstname.nil?
   end
 
   def information()
-    puts "id: #{@id}, surname: #{@surname}, firstname: #{@firstname}, lastname: #{@lastname}, phone_number: #{@phone_number}, telegram: #{@telegram}, email: #{@email}, git: #{@git}"
+    puts
+    puts "ID: #{@id}"
+    puts "Surname: #{@surname}"
+    puts "Firstname: #{@firstname}"
+    puts "Lastname: #{@lastname}"
+    puts "Phone_number: #{@phone_number}"
+    puts "Telegram: #{@telegram}"
+    puts "Email: #{@email}"
+    puts "Git: #{@git}"
   end
 
 end
--- Student/main.rb
+++ Student/main.rb
@@ -1,7 +1,7 @@
 require './class_student.rb'
 
-student1 = Student.new("Агаджанян", "Алёна", "Самвеловна", 1, "81234567895", "@alena", "agadzhanyan_alyona@mail.ru", "github.com/AlenaAgadzhanyan")
-student2 = Student.new("Аникин", "Марк", "Андреевич", 2, "89235614578", "@mark", "anikin_mark@gmail.com", "github.com/Corray333")
+student1 = Student.new(surname: "Агаджанян", firstname: "Алёна", lastname: "Самвеловна", id: 1, phone_number: "81234567895", telegram: "@alena", email: "agadzhanyan_alyona@mail.ru", git: "github.com/AlenaAgadzhanyan")
+student2 = Student.new(surname: "Аникин", firstname: "Марк", lastname: "Андреевич", id: 2, phone_number: "89235614578", telegram: "@mark", email: "anikin_mark@gmail.com", git: "github.com/Corray333")
 
 student1.information()
 student2.information()
