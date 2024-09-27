140b2dde07a7d41261892c8677b687c4a1ce8b35
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-09-27 20:39:09 +0300

    task 1 subtask 10

--- Student/class_student.rb
+++ Student/class_student.rb
@@ -7,21 +7,12 @@ class Student
     self.surname = parameters[:surname]
     self.firstname = parameters[:firstname] 
     self.lastname = parameters[:lastname]
-    self.phone_number = parameters[:phone_number]
-    self.telegram = parameters[:telegram]
-    self.email = parameters[:email]
+    set_contacts(parameters)
     self.git = parameters[:git]
   end
 
   def self.phone_number_regex?(phone_number_value)
     phone_number_value.nil? || phone_number_value.match?(/\A(\+?7|8)\d{10}\z/)
-  end
-
-  def phone_number=(phone_number_value)
-    if !Student.phone_number_regex?(phone_number_value) then
-      raise "Incorrect phone number"
-    end
-    @phone_number = phone_number_value
   end
 
   def self.surname_firstname_lastname_regex?(surname_firstname_lastname_value)
@@ -56,22 +47,8 @@ class Student
     telegram_value.nil? || telegram_value.match?(/\A@[a-zA-Z0-9_]{5,32}\z/)
   end
 
-  def telegram=(telegram_value)
-    if !self.class.telegram_regex?(telegram_value) then
-      raise "Incorrect telegram"
-    end
-    @telegram = telegram_value
-  end
-
   def self.email_regex?(email_value)
     email_value.nil? || email_value.match?(/\A[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+\z/)
-  end
-
-  def email=(email_value)
-    if !self.class.email_regex?(email_value) then
-      raise "Incorrect email"
-    end
-    @email = email_value
   end
 
   def self.git_regex?(git_value)
@@ -87,6 +64,12 @@ class Student
 
   def validate?
     return Student.git_regex(@git) && (Student.email_regex(@email) || Student.phone_number_regex(@phone_number) || Student.telegram_regex(@telegram))
+  end
+
+  def set_contacts(parameters = {})
+    @phone_number = parameters[:phone_number] if Student.phone_number_regex?(parameters[:phone_number])
+    @email = parameters[:email] if Student.email_regex?(parameters[:email])
+    @telegram = parameters[:telegram] if Student.telegram_regex?(parameters[:telegram])
   end
 
   def to_s()
