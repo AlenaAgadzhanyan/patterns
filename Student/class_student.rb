3d3bf2ad73af717508961ce931da8a5dc87fb59f
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-09-27 20:24:40 +0300

    task 1 subtask 9

--- Student/class_student.rb
+++ Student/class_student.rb
@@ -14,23 +14,23 @@ class Student
   end
 
   def self.phone_number_regex?(phone_number_value)
-    phone_number_value.match(/\A(\+?7|8)\d{10}\z/)
+    phone_number_value.nil? || phone_number_value.match?(/\A(\+?7|8)\d{10}\z/)
   end
 
   def phone_number=(phone_number_value)
-    if !self.class.phone_number_regex?(phone_number_value) then
+    if !Student.phone_number_regex?(phone_number_value) then
       raise "Incorrect phone number"
     end
     @phone_number = phone_number_value
   end
 
   def self.surname_firstname_lastname_regex?(surname_firstname_lastname_value)
-    surname_firstname_lastname_value.match(/^[А-ЯЁа-яё]+$/)
+    surname_firstname_lastname_value.match?(/^[А-ЯЁа-яё]+$/)
   end
 
   def surname=(surname_value)
     if surname_value.nil? then raise "The fields of the surname, firstname, lastname must be filled in"
-    elsif !self.class.surname_firstname_lastname_regex?(surname_value) then
+    elsif !Student.surname_firstname_lastname_regex?(surname_value) then
       raise "Incorrect surname"
     end
     @surname = surname_value[0].upcase + surname_value.slice(1, surname_value.length - 1)
@@ -38,7 +38,7 @@ class Student
 
   def firstname=(firstname_value)
     if firstname_value.nil? then raise "The fields of the surname, firstname, lastname must be filled in"
-    elsif !self.class.surname_firstname_lastname_regex?(firstname_value) then
+    elsif !Student.surname_firstname_lastname_regex?(firstname_value) then
       raise "Incorrect firstname"
     end
     @firstname = firstname_value[0].upcase + firstname_value.slice(1, firstname_value.length - 1)
@@ -46,14 +46,14 @@ class Student
 
   def lastname=(lastname_value)
     if lastname_value.nil? then raise "The fields of the surname, firstname, lastname must be filled in"
-    elsif !self.class.surname_firstname_lastname_regex?(lastname_value) then
+    elsif !Student.surname_firstname_lastname_regex?(lastname_value) then
       raise "Incorrect lastname"
     end
     @lastname = lastname_value[0].upcase + lastname_value.slice(1, lastname_value.length - 1)
   end
 
   def self.telegram_regex?(telegram_value)
-    telegram_value.match(/\A@[a-zA-Z0-9_]{5,32}\z/)
+    telegram_value.nil? || telegram_value.match?(/\A@[a-zA-Z0-9_]{5,32}\z/)
   end
 
   def telegram=(telegram_value)
@@ -64,7 +64,7 @@ class Student
   end
 
   def self.email_regex?(email_value)
-    email_value.match(/\A[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+\z/)
+    email_value.nil? || email_value.match?(/\A[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+\z/)
   end
 
   def email=(email_value)
@@ -75,7 +75,7 @@ class Student
   end
 
   def self.git_regex?(git_value)
-    git_value.match(/\A(https:\/\/)?github.com\/[a-zA-Z0-9_-]+\z/)
+    git_value.nil? || git_value.match?(/\A(https:\/\/)?github.com\/[a-zA-Z0-9_-]+\z/)
   end
 
   def git=(git_value)
@@ -85,16 +85,20 @@ class Student
     @git = git_value
   end
 
-  def information()
+  def validate?
+    return Student.git_regex(@git) && (Student.email_regex(@email) || Student.phone_number_regex(@phone_number) || Student.telegram_regex(@telegram))
+  end
+
+  def to_s()
     puts
     puts "ID: #{@id}"
     puts "Surname: #{@surname}"
     puts "Firstname: #{@firstname}"
     puts "Lastname: #{@lastname}"
-    puts "Phone_number: #{@phone_number}"
-    puts "Telegram: #{@telegram}"
-    puts "Email: #{@email}"
-    puts "Git: #{@git}"
+    puts "Phone_number: #{@phone_number}" if @phone_number
+    puts "Telegram: #{@telegram}" if @telegram
+    puts "Email: #{@email}" if @email
+    puts "Git: #{@git}" if @git
   end
 
 end
--- Student/main.rb
+++ Student/main.rb
@@ -3,5 +3,5 @@ require './class_student.rb'
 student1 = Student.new(surname: "Агаджанян", firstname: "Алёна", lastname: "Самвеловна", id: 1, phone_number: "81234567895", telegram: "@alena", email: "agadzhanyan_alyona@mail.ru", git: "github.com/AlenaAgadzhanyan")
 student2 = Student.new(surname: "аникин", firstname: "марк", lastname: "андреевич", id: 2, phone_number: "89235614578", telegram: "@mark333", email: "anikin_mark@gmail.com", git: "github.com/Corray333")
 
-student1.information()
-student2.information()
+student1.to_s()
+student2.to_s()
