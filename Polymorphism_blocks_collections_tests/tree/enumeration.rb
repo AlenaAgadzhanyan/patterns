5989b787a1f7311ad94044f7d69fa9b09098aa43
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-12-13 21:35:16 +0300

    lab 3 task 3: added Tag, HTMLTree, BFS, DFS classes

---
+++ Polymorphism_blocks_collections_tests/tree/enumeration.rb
@@ -0,0 +1,45 @@
+class TreeIterator
+  include Enumerable
+
+  attr_reader :root
+
+  def initialize(root)
+    @root = root
+  end
+
+  def each(&block)
+    enumerator.each(&block)
+  end
+
+  protected
+
+  def enumerator
+    raise NotImplementedError, 'Must implement enumerator in subclasses'
+  end
+end
+
+class DFS < TreeIterator
+  def enumerator
+    Enumerator.new do |yielder|
+      stack = [@root]
+      until stack.empty?
+        current = stack.pop
+        yielder << current unless current.name == "root"
+        stack.concat(current.children.reverse) unless current.children.empty?
+      end
+    end
+  end
+end
+
+class BFS < TreeIterator
+  def enumerator
+    Enumerator.new do |yielder|
+      queue = [@root]
+      until queue.empty?
+        current = queue.shift
+        yielder << current unless current.name == "root"
+        queue.concat(current.children) unless current.children.empty?
+      end
+    end
+  end
+end
