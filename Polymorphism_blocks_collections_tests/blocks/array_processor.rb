1d11082d3ad190b693ad5cb337ca9b7495c80b87
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2024-12-13 19:00:37 +0300

    lab 3 task 2.2: add method find_all

--- Polymorphism_blocks_collections_tests/blocks/array_processor.rb
+++ Polymorphism_blocks_collections_tests/blocks/array_processor.rb
@@ -2,7 +2,7 @@ class ArrayProcessor
 	def initialize(array)
 		self.array = array.dup
 	end
-  
+
 	def any?
 		self.array.each do |element|
 			return true if yield(element)
@@ -10,6 +10,14 @@ class ArrayProcessor
 		false
 	end
 
+  def find_all
+		result = []
+		self.array.each do |element|
+			result << element if yield(element)
+		end
+		result
+	end
+  
 	def to_a
 		self.array.dup
 	end
