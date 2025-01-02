921922925ff0155871e52b560f698db4a2b7ee9c
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2025-01-02 15:34:26 +0300

    lab4 task 1.3-1.4: create a class DataList and methods implemented

---
+++ Student/data_list/data_list.rb
@@ -0,0 +1,44 @@
+class DataList
+  def initialize(data)
+    @data = data.sort.freeze
+    @selected = []
+  end
+
+  def sekect(number)
+    @selected << number unless @selected.include?(number)
+  end
+
+  def get_selected()
+    @selected.dup
+  end
+
+  def get_names()
+    raise ArgumentError, "The method is not implemented"
+  end
+
+  def get_data()
+    raise ArgumentError, "The method is not implemented"
+  end
+
+  private
+
+  attr_reader :data
+  attr_accessor :selected
+
+  def data=(data)
+    @data = data.map{|row| deep_copy(row)}
+  end
+
+  def deep_copy(obj)
+    if obj.is_a?(Array)
+      obj.map { |item| deep_copy(item) }
+    elsif obj.is_a?(Hash)
+      obj.transform_values { |value| deep_dup(value) }
+    elsif obj.respond_to?(:dup)
+      obj.dup
+    else
+      obj
+    end
+  end
+
+end
