181eca288bf54e244fcc0334e95bc6a1da8b102f
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2025-01-02 19:13:31 +0300

    lab4 task 1.8: testing

--- Student/data_list/data_list_student_short.rb
+++ Student/data_list/data_list_student_short.rb
@@ -1,5 +1,5 @@
-require './data_list.rb'
-require '../data_table/data_table.rb'
+require_relative 'data_list.rb'
+require_relative '../data_table/data_table.rb'
 
 class DataListStudentShort < DataList
 
