e14f9a89639ae27647940620e0985902aa936463
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2025-01-02 13:48:52 +0300

    lab4 task1 subtask1: create a class DataTable

---
+++ Student/data_table/data_table.rb
@@ -0,0 +1,17 @@
+class DataTable
+  private attr_reader: data
+
+  def initialize(data)
+    @data = data
+  end
+
+  def data=(data)
+    raise ArgumentError, "Incorrect data" unless valid_data?(data)
+    @data = data.map(|row| row.dup)
+  end
+
+  def valid_data?(data)
+    data.is_a(Array) && data.all? {|row| row.is_a(Array)} 
+  end
+
+end
