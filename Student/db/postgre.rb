51bf62c54b01f3fea380c65ebaccb87ee7d4a8f3
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2025-01-21 14:03:50 +0300

    lab 4 task 3.1-3.5: the db has been created and filled wiht data; select request created

---
+++ Student/db/postgre.rb
@@ -0,0 +1,20 @@
+require 'pg'
+
+class PG_client
+  def initialize
+    self.client = PG.connect(
+      dbname: 'Students',
+      user: 'postgres',
+      password: '123',
+      host: 'localhost',
+      port: 5432
+    )
+  end
+
+  def exec(query)
+    client.exec(query)
+  end
+  
+  private
+  attr_accessor :client
+end
