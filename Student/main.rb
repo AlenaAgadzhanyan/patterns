934966756cd84b30dea529ee5956f908ce3d1f2e
AlenaAgadzhanyan <agadzhanyan_alyona@mail.ru>
2025-01-21 14:01:00 +0300

    lab4 task 3.5: select request created

--- Student/main.rb
+++ Student/main.rb
@@ -1,8 +1,9 @@
-require './student.rb'
-require './student_short.rb'
+require './entities/student.rb'
+require './entities/student_short.rb'
 require './student_tree.rb'
 require './data_list/data_list.rb'
 require './data_list/data_list_student_short.rb'
+require './db/postgre.rb'
 
 # student1 = Student.new(surname: "Агаджанян", firstname: "Алёна", lastname: "Самвеловна", id: 1, phone_number: "81234567895", telegram: "@alena", email: "agadzhanyan_alyona@mail.ru", git: "github.com/AlenaAgadzhanyan", birth_date: "2004.06.10")
 # student2 = Student.new(surname: "аникин", firstname: "марк", lastname: "андреевич", id: 2,  telegram: "@mark333", phone_number: "89235614578", email: "anikin_mark@gmail.com", git: "github.com/Corray333", birth_date: "2002.10.12")
@@ -21,13 +22,16 @@ require './data_list/data_list_student_short.rb'
 
 # tree.each{|student| puts student.to_s}
 
-test = DataListStudentShort.new([
-  Student_short.new(id:1, fio:"Ivanov I. I.", contact:"ivanov227@gmail.com",  git:"github.com/ivanov"),
-  Student_short.new(id:2, fio:"Petrov P. P.", contact:"@test_tgk",  git:"github.com/wsed4"),
-  Student_short.new(id:3, fio:"Sidorov S. S.", contact:"89320509129",  git:"github.com/dhy49")
-])
+# test = DataListStudentShort.new([
+#   Student_short.new(id:1, fio:"Ivanov I. I.", contact:"ivanov227@gmail.com",  git:"github.com/ivanov"),
+#   Student_short.new(id:2, fio:"Petrov P. P.", contact:"@test_tgk",  git:"github.com/wsed4"),
+#   Student_short.new(id:3, fio:"Sidorov S. S.", contact:"89320509129",  git:"github.com/dhy49")
+# ])
 
-puts test.get_data().get_element(0, 2) 
-test.select(1)
-test.select(2)
-test.get_selected.each { |item| puts item.to_s }
+# puts test.get_data().get_element(0, 2) 
+# test.select(1)
+# test.select(2)
+# test.get_selected.each { |item| puts item.to_s }
+
+client = PG_client.new
+client.exec("select * from students;").each { |row| puts row }
